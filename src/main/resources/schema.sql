077aff33ce293da76ed92e0e38688890fb7445bb
Dh2h7k8 <dhoot_anshul@gap.com>
2017-04-12 21:54:36 +0530

    s

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,4 +6,28 @@ CREATE TABLE car (
 	year int,
 	PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS `acc_user`;
+CREATE TABLE `acc_user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`,`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `card_details`
+--
+
+DROP TABLE IF EXISTS `card_details`;
+CREATE TABLE `card_details` (
+  `card_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `card_number` int(16) unsigned NOT NULL,
+  `card_name` varchar(255) DEFAULT NULL,
+  `start_date` varchar(5) DEFAULT NULL,
+  `expire_date` varchar(5) DEFAULT NULL,
+  `user_id` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`card_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `acc_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 */
